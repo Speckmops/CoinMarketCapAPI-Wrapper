b5d520a570c980c9b083f42ba3e307ee95d2f25c
Stefan <git@0x1911.com>
2017-11-09 12:00:43 +0100

    Wordfix

--- CoinMarketCapAPI.cs
+++ CoinMarketCapAPI.cs
@@ -29,7 +29,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Net;
 using System.Text.RegularExpressions;
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("CoinMarketCapAPI")]
-[assembly: AssemblyDescription("API for CoinMarketCap")]
+[assembly: AssemblyDescription("API-Wrapper for CoinMarketCap")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stefan Zehnpfennig")]
 [assembly: AssemblyProduct("CoinMarketCapAPI")]
